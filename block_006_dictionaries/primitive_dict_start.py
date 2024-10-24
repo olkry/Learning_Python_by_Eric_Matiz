603a265d705ad1b835bb0d864f1df6915b661fc9
olkry <adenos90@gmail.com>
2024-10-24 13:56:26 +0300

    block 006 contin

---
+++ block_006_dictionaries/primitive_dict_start.py
@@ -0,0 +1,9 @@
+human = {
+    'first_name': "Gasty",
+    'last_name': 'Mareno',
+    'city': 'Moscow'
+}
+
+print(human['first_name'])
+print(human['last_name'])
+print(human.get('city', 'City is not available'))
