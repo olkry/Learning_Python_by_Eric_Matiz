8cf91a7d833c793c0611f50f7d350b973c4c3641
olkry <adenos90@gmail.com>
2024-10-29 16:43:46 +0300

    block 006 fin

--- block_006_dictionaries/favorite_numbers.py
+++ block_006_dictionaries/favorite_numbers.py
@@ -1,13 +1,18 @@
 friends = {
-    'Herman': 108,
-    'Cloud': 1,
-    'Sephiroth': 42,
-    "Gabriel": 192,
-    "Alucard": 667
+    'Herman': [108, 69],
+    'Cloud': [1, 7],
+    'Sephiroth': [42, 16, 38],
+    "Gabriel": [192],
+    "Alucard": [667, 665]
     }
 
-favorite_numbers = (f'Herman - {friends['Herman']} \nCloud - {friends['Cloud']} '
-                    f'\nSephiroth - {friends['Sephiroth']} \nGabriel - {friends["Gabriel"]} '
-                    f'\nAlucard - {friends["Alucard"]}')
+# favorite_numbers = (f'Herman - {friends['Herman']} \nCloud - {friends['Cloud']} '
+#                     f'\nSephiroth - {friends['Sephiroth']} \nGabriel - {friends["Gabriel"]} '
+#                     f'\nAlucard - {friends["Alucard"]}')
+#
+# print(favorite_numbers)
 
-print(favorite_numbers)
+for name, numbers in friends.items():
+    print(f'\nУ {name.title()} любимые числа:')
+    for numb in numbers:
+        print(f'-\t{numb}')
--- block_006_dictionaries/primitive_dict_start.py
+++ block_006_dictionaries/primitive_dict_start.py
@@ -1,9 +1,26 @@
+
+
 human = {
     'first_name': "Gasty",
     'last_name': 'Mareno',
     'city': 'Moscow'
 }
+human_2 = {
+    'first_name': "Salvador",
+    'last_name': 'Dali',
+    'city': 'Barcelona'
+}
+human_3 = {
+    'first_name': "Herman",
+    'last_name': 'Hesse',
+    'city': 'Berlin'
+}
+people = [human, human_2, human_3]
 
-print(human['first_name'])
-print(human['last_name'])
-print(human.get('city', 'City is not available'))
+# print(human['first_name'])
+# print(human['last_name'])
+# print(human.get('city', 'City is not available'))
+
+for person in people:
+    print(f'Name: {person['first_name']} {person['last_name']}')
+    print(f'Hometown: {person['city']}\n')
